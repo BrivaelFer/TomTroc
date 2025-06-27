78ac9d6fffb3fb33d963a1d9ba02c52bfa4423b5
BrivaelFer <77669345+BrivaelFer@users.noreply.github.com>
2025-06-27 12:08:20 +0200

    render secondary

--- View/View.php
+++ View/View.php
@@ -32,14 +32,6 @@ class View
         echo ob_get_clean();
     }
 
-    public function renderSecondary(string $viewPath, array $params = []) : void
-    {
-        extract($params); // On transforme les diverses variables stockées dans le tableau "params" en véritables variables qui pourront être lues dans le template.
-        ob_start();
-        require($viewPath);
-        echo ob_get_clean();
-    }
-
     private function _renderViewFromTemplate(string $viewPath, array $params = []) : string
     {  
         if (file_exists($viewPath)) {
